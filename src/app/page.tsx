6de683e574a8677e65c3646336721c591982ef80
Sergey Eroshenkov <itechmeat@gmail.com>
2024-05-31 06:34:00 +0200

    Init the project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'TonPass - your ticket to the world of events!',
+  description:
+    'TonPass - your all-in-one tool for organizing and attending events. Buy tickets, pay with cryptocurrency, and receive unique NFTs with QR codes.',
+}
+
+export default function HomePage() {
+  return <div>HOMEPAGE</div>
+}
